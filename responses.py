2ec91ae3f86593f261a6549843ca777debff8264
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-20 11:53:24 +0000

    open ai added but still need to buys credits

--- responses.py
+++ responses.py
@@ -1,3 +1,19 @@
+import openai
+import os
+
+openai.api_key = os.environ['API_KEY']
 def handle_response(message) -> str:
-    print(message)
-    return "hello there you passed " + message
+
+    model_engine = "text-davinci-002"
+    prompt = f"{message}\n"
+    
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    ).choices[0].text
+    
+    return response
