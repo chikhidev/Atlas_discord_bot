b6c77d0f34dd1b53e37f9fa4cea4320d9f2f370e
Abderrahim Chikhi <chikhi.dev@gmail.com>
2023-04-20 11:56:16 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,11 @@
 import discord
 import responses
-
-async def send_message(message, user_message, is_private):
-    try:
-        response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else message.channel.send(response)
-
-    except Exception as e:
-        await message.author.send(e)
-
-
-
+import os
 
 
 
 def run_discord_bot():
+
     TOKEN = os.environ['TOKEN']
     intents = discord.Intents.default()
     client = discord.Client(intents=intents)
@@ -32,13 +23,32 @@ def run_discord_bot():
         user_name = str(message.author),
         user_message = str(message.content)
         channel = str(message.channel)
-
+        print("message :",message)
         print(f"{user_name} said {user_message} in {channel}")
 
-        if user_message[0] == '?':
+        if user_message and user_message[0] == "?":
             user_message = user_message[1:]
             await send_message(message, user_message, is_private = True)
         else:
             await send_message(message, user_message, is_private = False)
     
     client.run(TOKEN)
+
+
+
+
+
+async def send_message(message, user_message, is_private):
+    print("message is: ", user_message)
+    try:
+        response = responses.handle_response(user_message)
+    
+        if is_private:
+            await message.author.send(response)
+        else :
+            await message.channel.send(response)
+
+
+    except Exception as e:
+        await message.author.send(e)
+
